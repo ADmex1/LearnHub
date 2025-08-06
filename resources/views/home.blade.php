4ebccdc227ab2a80e3f76839a00fea714025886a
Viktor <157784316+ADmex1@users.noreply.github.com>
2025-08-07 02:47:38 +0800

    Mini changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,4 @@
 <x-layout :title=$title>
-    <h1 class ="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">Hello and welcome to my website</h1>
-    <p class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Possimus
-        doloremque ex, ullam vitae sed voluptatem
-        repellat itaque enim! Iste, iure corporis magni ullam vero veritatis enim maiores quibusdam odio dicta?</p>
+    <h1 class ="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">Hello and welcome to LearnHub</h1>
+    <p class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">This is LearnHub Not PornHub</p>
 </x-layout>
